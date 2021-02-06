0329035c9ed41c024fe727968c9fd90356d7c8fe
doberman <doberman24@yandex.ru>
2021-02-07 00:17:26 +0300

    Добавил динамическуие светлую и темную темы

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+let page = document.querySelector('.page');
+let changeMode = document.querySelector('.change-mode');
+
+changeMode.onclick = function() {
+    page.classList.toggle('dark-theme');
+}
